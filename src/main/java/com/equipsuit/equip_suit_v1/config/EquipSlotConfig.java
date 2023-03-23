2e04366d410d4a5c9540edc62e39cdb8134daf09
1121665965 <1121665965@qq.com>
2023-03-24 00:24:05 +0800

    forge1.19_api

--- src/main/java/com/equipsuit/equip_suit_v1/config/EquipSlotConfig.java
+++ src/main/java/com/equipsuit/equip_suit_v1/config/EquipSlotConfig.java
@@ -12,9 +12,9 @@ public class EquipSlotConfig {
     public static final ForgeConfigSpec.ConfigValue<List<? extends Integer>> EQUIP_SLOT_LIST;
     static {
         BUILDER.push("equip slot config");
-        Predicate<Object> Integers= integers -> (Integer)integers > 0 && (Integer)integers < 64;
-        NonNullList<Integer> EquipSlotNum = NonNullList.of(40,39,38,37,36);
-        EQUIP_SLOT_LIST=BUILDER.comment("add your custom slot nums,make sure  the number exists in your inventory slots list").defineList("equip list", EquipSlotNum ,Integers);
+        Predicate<Object> Integers= integers -> (Integer)integers >= 0 && (Integer)integers < 64;
+        NonNullList<Integer> EquipSlotNum = NonNullList.of(64,39,38,37,36);
+        EQUIP_SLOT_LIST=BUILDER.comment("add your custom slot nums,make sure the number exists in your inventory slots list,\nrange:[0,64)").defineList("equip list", EquipSlotNum ,Integers);
         BUILDER.pop();
         SPEC=BUILDER.build();
     }
--- src/main/java/com/equipsuit/equip_suit_v1/utils/EquipSuitHelper.java
+++ src/main/java/com/equipsuit/equip_suit_v1/utils/EquipSuitHelper.java
@@ -8,12 +8,6 @@ import net.minecraft.world.item.ItemStack;
 import java.util.List;
 
 public class EquipSuitHelper{
-    public static void SuitChange(Player player){
-        SuitChange(player,new ContainerEquipSuit(player.getInventory(),9,10,11,12));
-    }
-    public static void SuitChangeWithoutOff(Player player){
-        SuitChangeWithoutOff(player,new ContainerEquipSuit(player.getInventory(),9,10,11,12));
-    }
     public static void SuitChange(Player player, EquipSuit suit){
         List<ItemStack> list=List.copyOf(suit.getSlotItems());
         for(int i=0;i<list.size();i++){
